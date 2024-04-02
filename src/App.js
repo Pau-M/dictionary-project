2b21a8572e3de3848ba6ee675916eeb1970f964a
Pau <mozga.paulina@gmail.com>
2024-04-02 19:55:29 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Dictionary Project</header>
+      <a
+        href="https://www.shecodes.io/learn/cohorts/1767/units/69/lessons/254"
+        className="btn btn-primary"
+      >
+        Click
+      </a>
+    </div>
+  );
+}
+
+export default App;
