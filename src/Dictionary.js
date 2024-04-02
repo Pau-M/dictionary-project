66c0913a09a18017826d9122f3f271bece8ff682
Pau <mozga.paulina@gmail.com>
2024-04-02 21:05:11 +0200

    adding API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyWord, setKeyWord] = useState("");
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyWord}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function keyWordChange(event) {
     setKeyWord(event.target.value);
